3765f670f5bfdd39d3e0efea908cfcd26b0b4be2
Abdulaziz-Hassan <abdulazizhza@gmail.com>
2023-11-08 23:12:34 +0200

    Add UCS, Greedy, and A* implementations with their respective queues
    Co-authored-by: Ahmed Nasser <ahmednasser217217@gmail.com>
    Co-authored-by: Aya Fayed <ayaa_fayed@yahoo.com>

--- src/code/LLAPSearch.java
+++ src/code/LLAPSearch.java
@@ -1,14 +1,18 @@
 package code;
 
 import code.actions.Action;
-import code.search_queues.BFSQueue;
-import code.search_queues.DFSQueue;
-import code.search_queues.SearchQueue;
+import code.actions.BuildAction;
+import code.heuristics.FirstHeuristic;
+import code.heuristics.Heuristic;
+import code.logger.Logger;
+import code.search_queues.*;
 
 import java.util.List;
 
 public class LLAPSearch extends GenericSearch {
+
     private static Parser parser = new Parser();
+
     public LLAPSearch() throws Exception {
         super();
         throw new Exception("Static Class!");
@@ -17,19 +21,26 @@ public class LLAPSearch extends GenericSearch {
     public static String solve(String initialState, String strategy, boolean visualize) {
         State state = parser.parseState(initialState);
         List<Action> actions = parser.parseActionsList(initialState);
-        SearchQueue searchQueue = getSearchQueue(strategy);
-        return generalSearch(state,actions, searchQueue, strategy.equals("ID"));
+        List<BuildAction> buildActions = actions.stream()
+                .filter(action -> action instanceof BuildAction)
+                .map(action -> (BuildAction) action)
+                .toList();
+        SearchQueue searchQueue = getSearchQueue(strategy, buildActions);
+        return generalSearch(state,actions, searchQueue, strategy.equals("ID"), visualize);
     }
 
-    public static SearchQueue getSearchQueue(String strategy) {
-        switch (strategy) {
-            case "BF":
-                return new BFSQueue();
-            case "DF":
-            case "ID":
-                return new DFSQueue();
-
-        }
-        return null;
+    public static SearchQueue getSearchQueue(String strategy, List<BuildAction> buildActions) {
+        Heuristic firstHeuristic = new FirstHeuristic(buildActions);
+        Heuristic secondHeuristic = null; // TODO: handle second heuristic
+        return switch (strategy) {
+            case "BF" -> new BFSQueue();
+            case "DF", "ID" -> new DFSQueue();
+            case "UC" -> new UCSQueue();
+            case "GR1" -> new GRQueue(firstHeuristic);
+            case "GR2" -> new GRQueue(firstHeuristic);
+            case "AS1" -> new AStarQueue(firstHeuristic);
+            case "AS2" -> new AStarQueue(firstHeuristic);
+            default -> null;
+        };
     }
 }
